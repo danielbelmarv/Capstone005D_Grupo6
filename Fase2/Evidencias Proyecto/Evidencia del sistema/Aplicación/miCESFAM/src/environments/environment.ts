3189fabbf24d91e908954bfaf2766abf1c485708
alvarofelipe2909 <al.salinasf@duocuc.cl>
2024-10-25 17:17:24 -0300

    Update environment.ts

--- Fase2/Evidencias Proyecto/Evidencia del sistema/Aplicación/miCESFAM/src/environments/environment.ts
+++ Fase2/Evidencias Proyecto/Evidencia del sistema/Aplicación/miCESFAM/src/environments/environment.ts
@@ -5,16 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const environment = {
-  production : false,
-  firebaseConfig : {
-    apiKey: "AIzaSyD8z7t30vF9-7CYGNujq5IKVo9vw-2c1qw",
-    authDomain: "micesfam-39406.firebaseapp.com",
-    databaseURL: "https://micesfam-39406-default-rtdb.firebaseio.com",
-    projectId: "micesfam-39406",
-    storageBucket: "micesfam-39406.appspot.com",
-    messagingSenderId: "899301906929",
-    appId: "1:899301906929:web:62214fdfd6f1f716144c50"
-  }
+
 };
 
 // Initialize Firebase
